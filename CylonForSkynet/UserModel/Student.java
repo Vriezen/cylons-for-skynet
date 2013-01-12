695e63f3dd8307eaa16061b63c3b240531920b11
anorby <anorby@msp-anorby-w7a.MSC.local>
2013-01-11 22:47:28 -0600

    Here we go again....

---
+++ CylonForSkynet/UserModel/Student.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+
+public class Student extends User {
+
+	public Student() {}
+	public Student(String firstName, String lastName, String userName, int profVal, List<String> langs)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName = userName;
+		this.levelOfProficiency = profVal;
+		this.friendsList = null;
+		this.languageList = langs;
+		this.userLanguageGreeting = "Hello, "; //Will need to change this to query database for correct greeting!
+	}
+
+}
